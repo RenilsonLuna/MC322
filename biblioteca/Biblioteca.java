27011b3090d66e41315fd25ce801acbc29b804c2
RenilsonLuna <61640234+RenilsonLuna@users.noreply.github.com>
2023-08-15 16:50:34 -0300

    Classe implementada
    Atributos correspondentes à classe adicionados e métodos remover e adicionar livros implementados

--- biblioteca/Biblioteca.java
+++ biblioteca/Biblioteca.java
@@ -1,28 +1,67 @@
 package biblioteca;
 
+import biblioteca.Livro;
 
-public class biblioteca {
-    private String nomeBiblioteca;
-    private int nTotalLivros;
-    private int nLivrosEmprestados;
-    
-    /* 
-    será que realmente é necessário esse método? Visto que funcionario que chama pra emprestar o livro
-    public void emprestarLivro(Usuario usuario, Livro livro) {
-        // Lógica para empréstimo do livro
-        // Exemplo: atualizar status do livro, registrar o empréstimo para o usuário, etc.
-    }
-    
+public class Biblioteca {
 
-    public void devolverLivro(Usuario usuario, Livro livro) {
-        // Lógica para devolução do livro
-        // Exemplo: atualizar status do livro, registrar a devolução, etc.
+    private String nome;
+    private String cnpj;
+    private Livro[] livros = new Livro[999999];
+    private int qtdLivros = 0;
+
+    // Getters
+
+    public String getNome(){
+        return nome;
     }
 
-    */
+    public String getCnpj(){
+        return cnpj;
+    }
 
-    // public void setNEmprestLivro(int nEmprestLivro) {
-    //     this.nEmprestLivro = nEmprestLivro+1;
-    // }
+    public int getQtdLivros(){
+        return qtdLivros;
+    }
+
+
+    // Setters
+    public void setQtdLivros(int qtd){
+        this.qtdLivros = qtd;
+    }
+
+    public Biblioteca(String nome, String cnpj){
+        this.nome = nome;
+        this.cnpj = cnpj;
+    }
+
+    public void adicionarLivro(Livro livro){
+        int n_livros = getQtdLivros();
+        if (n_livros == 0){
+            livros[0] = livro;
+        }else{
+            livros[n_livros-1] = livro;
+        }
+        setQtdLivros(n_livros+1);
+        System.out.println(livro.getTitulo() + " adicionado! Agora a Lib possui " + getQtdLivros() + " Livros");
+    }
+
+    public void removerLivro(int tombo){
+        int n_livros = getQtdLivros();
+        for (int i = 0; i < n_livros; i++){
+            int t = livros[i].getTombo();
+
+            // Se achar o livro, troca o livro com o ultimo livro da fila e reduz qtd de livros 
+            // na pratica, torna posicao do livro no vetor inacessivel, pois um livro qualquer l ∈ [0, qtdLivros-1]
+            if (t == tombo){
+                Livro tmp = livros[i];
+                livros[i] = livros[n_livros-1];
+                livros[n_livros-1] = tmp;
+                setQtdLivros(n_livros-1);
+                System.out.println(tmp.getTitulo() + " Removido. Agora a biblioteca possui " + getQtdLivros() + " livros.");
+                return;
+            }
+        }
+        System.out.println("Livro nao encontrado!");
+    }
 
 }
