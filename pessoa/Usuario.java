4dbd0647407bf0c16064f99e2dc5b36979e2efd3
RenilsonLuna <61640234+RenilsonLuna@users.noreply.github.com>
2023-08-15 16:53:16 -0300

    Funções de solicitar emprestimo e dar baixa add

--- pessoa/Usuario.java
+++ pessoa/Usuario.java
@@ -4,52 +4,65 @@ import pessoa.Funcionario;
 
 public class Usuario {
     private String nome;
-    private String documento;
+    private String ra;
     private boolean bloqueado = false;
-    private int nLivrosEmprestados = 0;
     private Livro[] livrosEmprestados = new Livro[4];
+    private int nLivrosEmprestados = 0;
 
     	
-    public Usuario(String nome, String documento){
+    public Usuario(String nome, String ra){
         this.nome = nome;
-        this.documento = documento;
+        this.ra = ra;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDocumento() {
-        return documento;
-    }
-
-
-    public int getNLivrosEmprestados() {
-        return nLivrosEmprestados;
-    }
-
-    public boolean getBloqueado() {
-        return bloqueado;
-    }
-
+    
     public void solicitarEmprestimo(Livro livro, Funcionario funcionario) {
         if (getBloqueado()) {
             System.out.println("Usuário bloqueado. Não é possível solicitar empréstimo.");
             return;
         }
-
-        if (nLivrosEmprestados >= 4) { 
+        
+        if (getNLivrosEmprestados() >= 4) { 
             System.out.println("Limite de empréstimos excedido. Não é possível solicitar mais empréstimos.");
             return;
         }
         
         funcionario.realizarEmprestimo(this, livro);
-        livrosEmprestados[nLivrosEmprestados] = livro;
+        livrosEmprestados[getNLivrosEmprestados()] = livro;
         nLivrosEmprestados++;
     }
+    
+    public void devolverLivro(Livro livro, Funcionario func){
+        func.darBaixa(livro, this);
+        livrosEmprestados[nLivrosEmprestados - 1] = null;
+        nLivrosEmprestados--;
+    }
 
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getDocumento() {
+        return ra;
+    }
+    
+    
+    public int getNLivrosEmprestados() {
+        return nLivrosEmprestados;
+    }
+    
+    public boolean getBloqueado() {
+        return bloqueado;
+    }
+    
 
+    // Setters
     public void setNome(String nome) {
         this.nome = nome;
+    }
+    
+    public void setBloquado(boolean valor){
+        this.bloqueado = valor;
     }
 }
