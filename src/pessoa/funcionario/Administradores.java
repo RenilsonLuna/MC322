fdf64cb41ac2276eb62d1fcef0792db8c8b9452c
RenilsonLuna <ra247358@paralamas.lab.ic.unicamp.br>
2023-09-05 17:26:08 -0300

    Reserva add

--- src/pessoa/funcionario/Administradores.java
+++ src/pessoa/funcionario/Administradores.java
@@ -1,10 +1,12 @@
 package pessoa.funcionario;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import biblioteca.Biblioteca;
 import biblioteca.multimidia.Multimidia;
 import pessoa.Pessoa;
+
 
 public final class Administradores extends Funcionario {
     public Administradores(String nome, String endereco, String contato, Biblioteca lib){
@@ -16,8 +18,18 @@ public final class Administradores extends Funcionario {
 
     public void reservar(Biblioteca lib,String tipo, String nomeTitulo, Pessoa usuario){ }
 
-    public void aplicarMulta(Pessoa usuario, LocalDate dataDeHoje, Multimidia item){ }
+    public void aplicarMulta(Pessoa usuario, Multimidia item){ 
+        long diasPassados = ChronoUnit.DAYS.between(item.getDataEntrega(), LocalDate.now());
+        double multa = diasPassados * usuario.getValorMulta();
+        System.out.println("Sr(a)"+ usuario.getNome() + "foi multado com o valor de: " + multa + " pois ficou " + diasPassados + "em atraso");
+        usuario.setTotalMulta(multa);
+    }
 
-    // Missing manage info
-    // missing borrow
+    public void reservar(Pessoa usuario, Multimidia midia){ 
+        lib.AdicionarReserva(usuario, midia);
+    }
+ 
+    public void retirarMulta(Pessoa usuario){
+        usuario.setTotalMulta(0.00);
+    }
 }
