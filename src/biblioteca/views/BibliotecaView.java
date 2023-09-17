cb9fa84ea7de34175c201c60297d9ca0dc941247
RenilsonLuna <renilsonjunior1@gmail.com>
2023-09-17 20:39:46 -0300

    src/biblioteca/views/BibliotecaViewImpl.java
    Implementacao de funcionalidade de mostrar itens disponiveis

--- src/biblioteca/views/BibliotecaView.java
+++ src/biblioteca/views/BibliotecaView.java
@@ -2,10 +2,10 @@ package biblioteca.views;
 
 import java.util.List;
 
-import biblioteca.models.ItemMultimidia;
+import biblioteca.models.ItemMultimidiaImpl;
 
 public interface BibliotecaView {
-    void mostrarItensDisponiveis(List<ItemMultimidia> itens);
+    void mostrarItensDisponiveis(List<ItemMultimidiaImpl> itens);
     void mostrarEmprestimoStatus(boolean sucesso);
     void mostrarDevolucaoStatus(boolean sucesso);
 }
