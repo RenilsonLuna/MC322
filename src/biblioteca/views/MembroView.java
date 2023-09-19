14713a7a82700b556eb64bef1554db03535696ff
RenilsonLuna <renilsonjunior1@gmail.com>
2023-09-18 21:54:18 -0300

    Implementacao de adicao, remocao e listagem de membros

--- src/biblioteca/views/MembroView.java
+++ src/biblioteca/views/MembroView.java
@@ -2,9 +2,9 @@ package biblioteca.views;
 
 import java.util.List;
 
-import biblioteca.models.Membro;
+import biblioteca.models.MembroImpl;
 
 public interface MembroView {
-    void mostrarListaMembros(List<Membro> membros);
-    void mostrarDetalhesMembro(Membro membro);
+    void mostrarListaMembros(List<MembroImpl> membros);
+    void mostrarDetalhesMembro(MembroImpl membro);
 }
--- src/biblioteca/views/MembroViewImpl.java
+++ src/biblioteca/views/MembroViewImpl.java
@@ -3,7 +3,7 @@ package biblioteca.views;
 import java.util.List;
 
 import biblioteca.controllers.MembroController;
-import biblioteca.models.Membro;
+import biblioteca.models.MembroImpl;
 
 public class MembroViewImpl implements MembroView {
     private MembroController membroController;
@@ -13,12 +13,21 @@ public class MembroViewImpl implements MembroView {
     }
 
     @Override
-    public void mostrarListaMembros(List<Membro> membros) {
-        // Implementação da exibição da lista de membros
-    }
+    public void mostrarListaMembros(List<MembroImpl> membros) {
+        if(membros.isEmpty()){
+            System.out.println("Não há membros na biblioteca");
+        }
+        else
+            for (MembroImpl m : membros)
+            System.out.println(m.getRa() + ": " + m.getNome());
+    } 
 
     @Override
-    public void mostrarDetalhesMembro(Membro membro) {
-        // Implementação da exibição dos detalhes de um membro
+    public void mostrarDetalhesMembro(MembroImpl membro) {
+        System.out.println("---- Dados do membro -------");
+        System.out.println("Nome: " + membro.getNome());
+        System.out.println("Documento: " + membro.getRa());
+        System.out.println("Contato: " + membro.getContato());
+        System.out.println("Endereco: " + membro.getEndereco());
     }
 }
