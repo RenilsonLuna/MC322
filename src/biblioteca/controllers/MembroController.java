b997288acb237ad7054cddf324341dd2781620d5
RenilsonLuna <renilsonjunior1@gmail.com>
2023-09-18 21:53:22 -0300

    Adicao, remocao e listagem de membros adicionado

--- src/biblioteca/controllers/MembroController.java
+++ src/biblioteca/controllers/MembroController.java
@@ -1,12 +1,13 @@
 package biblioteca.controllers;
 
 import java.util.List;
+import java.util.Scanner;
 
 import biblioteca.models.MembroImpl;
 
 public interface MembroController {
     List<MembroImpl> listarMembros();
     MembroImpl buscarMembroPorIdentificacao(int identificacao);
-    boolean adicionarMembro(MembroImpl membro);
+    boolean adicionarMembro(Scanner scanner);
     boolean removerMembro(int identificacao);
 }
--- src/biblioteca/controllers/MembroControllerImpl.java
+++ src/biblioteca/controllers/MembroControllerImpl.java
@@ -1,9 +1,9 @@
 package biblioteca.controllers;
 
 import java.util.List;
+import java.util.Scanner;
 
-import biblioteca.models.Biblioteca;
-import biblioteca.models.MembroImpl;
+import biblioteca.models.*;
 
 public class MembroControllerImpl implements MembroController {
     private Biblioteca lib;
@@ -25,13 +25,67 @@ public class MembroControllerImpl implements MembroController {
         return null;
     }
 
-    public boolean adicionarMembro(MembroImpl membro){
-        try {
-            lib.cadastrarMembro(membro);
-            return true;
-        } catch (Exception e) {
-            return false;
+    public boolean adicionarMembro(Scanner scanner){
+
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Endereco: ");
+        String endereco = scanner.nextLine();
+
+        System.out.print("Contato: ");
+        String contato = scanner.nextLine();
+        
+        System.out.println("1. Atendente");
+        System.out.println("2. Gerente");
+        System.out.println("3. Administrador");
+        System.out.println("4. Graduacao");
+        System.out.println("5. Pos-Graduacao");
+        System.out.println("6. Professor");
+        
+        System.out.println();
+        System.out.print("Selecione o tipo de membro: ");
+        int tipoMembro = scanner.nextInt();
+        scanner.nextLine();
+
+        switch (tipoMembro) {
+            case 1:
+                Atendente a = new Atendente(nome, endereco, contato, lib);
+                lib.cadastrarMembro(a);
+                break;
+            case 2:
+                Gerente g = new Gerente(nome, tipoMembro, endereco, contato, lib);
+                lib.cadastrarMembro(g);
+                break;
+            case 3:
+                Administradores adm = new Administradores(nome, endereco, contato, lib);
+                lib.cadastrarMembro(adm);
+                break;
+            case 4:
+                Graduacao grad = new Graduacao(nome, endereco, contato, lib);
+                lib.cadastrarMembro(grad);
+                break;
+
+            case 5:
+                System.out.print("1. Doutorado\n2. Mestrado\n\nSelecione o grau: ");
+                int grau = scanner.nextInt();
+                scanner.nextLine();
+                StrictoSensu s;
+
+                if (grau == 1)
+                    s = StrictoSensu.Doutorado;
+                else    
+                    s = StrictoSensu.Mestrado;
+                
+                PosGraduacao pos = new PosGraduacao(s, contato, nome, endereco, contato, lib);
+                lib.cadastrarMembro(pos);
+
+
+            default:
+                break;
         }
+
+        return true;
     }
 
     public boolean removerMembro(int identificacao){
